ef82bb836f1859562b5b8485d7f3eedfc34f0b92
Aakrit Neupane <aakritneupane53@gmail.com>
2026-06-10 13:35:04 +0545

    patch updation schema added

--- Week-6-TodoAPI/src/Todo/routes/todo.routes.ts
+++ Week-6-TodoAPI/src/Todo/routes/todo.routes.ts
@@ -3,7 +3,7 @@ import express from "express";
 import validate from "../../middleware/validation.middleware";
 import authorize from "../../middleware/authorization.middleware";
 
-import { createToDoSchema } from "../schema/todo.schema";
+import { createToDoSchema, patchToDoSchema } from "../schema/todo.schema";
 
 import {
   fetchToDos,
@@ -21,7 +21,7 @@ router.post("/todos", validate(createToDoSchema), authorize, postToDos);
 router.get("/todos", fetchToDos);
 router.get("/todos/:id", fetchToDoByID);
 router.delete("/todos/:id", deleteToDoById);
-router.put("/todos/:id", updateToDoById);
-router.patch("/todos/:id", updateToDoByIdPatch);
+router.put("/todos/:id", validate(createToDoSchema), updateToDoById);
+router.patch("/todos/:id", validate(patchToDoSchema), updateToDoByIdPatch);
 
 export default router;
--- Week-6-TodoAPI/src/Todo/schema/patchTodo.schema.ts
+++ Week-6-TodoAPI/src/Todo/schema/patchTodo.schema.ts
@@ -1,13 +1,5 @@
 import { z } from "zod/v3";
 
-export const createToDoSchema = z.object({
-  title: z.string().min(5),
-  completed: z.boolean().default(false),
-  description: z.string().min(8),
-});
-
-export type CreateToDoDto = z.infer<typeof createToDoSchema>;
-
 export const patchToDoSchema = z.object({
   title: z.string().min(5).optional(),
   completed: z.boolean().default(false).optional(),
