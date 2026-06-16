f2c014e7cf25e8de1f53e758191f9e750de61940
Aakrit Neupane <aakritneupane53@gmail.com>
2026-06-16 15:29:40 +0545

    fix bug fetch event handler

--- Week-7-EMS/src/events/controller/event.controller.ts
+++ Week-7-EMS/src/events/controller/event.controller.ts
@@ -2,25 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 import { AppError } from "../../utils/AppError";
 import { fetchEvent, fetchEvents, postEvent } from "../services/event.services";
-import { eventDto } from "../schema/event.schema";
+import { eventDto, eventSchema } from "../schema/event.schema";
 import { success } from "zod";
-
-export async function fetchEventHandler(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
-  try {
-    const { id } = req.params as { id: string };
-    if (!id) throw new AppError("No id provided", 400);
-    const event = await fetchEvent(id);
-    if (!event) throw new AppError("Invalid event id", 400, event);
-    return res.status(200).json({ success: true, data: event });
-  } catch (error) {
-    console.log(`Error in fetchEventHandler ${error}`);
-    next(error);
-  }
-}
 
 export async function postEventHandler(
   req: Request,
